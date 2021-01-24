d8e433758b79250a6c483b2774fde70355d79098
yannis <yaanis213@gmail.com>
2021-01-24 14:38:44 +0100

    WIP, pushing to see diff in github website

---
+++ QuizTP/Scenes/Question/GeographicalQuizViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GeographicalQuizViewController.swift
+//  QuizTP
+//
+//  Created by Yannis MEKAOUCHE on 24/01/2021.
+//
+
+import UIKit
+
+class GeographicalQuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
